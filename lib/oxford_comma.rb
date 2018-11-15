d4c3a249db5fc5190a65626d461d8be5a3e70da2
FutureScript <fernandobaxter1@gmail.com>
2018-11-15 00:03:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
-  array.join
+  if array.length == 1 
+    array.join
   if array.length == 2
-    return array.join("and")
+    array.join("and")
   elsif
     array.length == 3
-    return array.join (",and")
+    array.join (",and")
   end
 end
 end
